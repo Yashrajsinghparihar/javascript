e06500f87d6c59fca667cc63efdcf356d57b6201
Yashraj Singh Parihar <yashrajsingh9340@gmail.com>
2026-09-17 11:19:23 +0530

    javascrip day1

---
+++ script.js
@@ -0,0 +1,37 @@
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+
+// console.log(a)
+// console.warn("something went wrong")
+// console.error("error 404")
+
+// var 
+
+var a = 10;
+console.log(a);
+var a = 20;
+// console.log(a)
+a = 21;
+console.log(a)
+
+// let b = 30;
+// b = 40;
+
+// console.log(b)
+
+// const f = 10;
+// f = 20;
+
+// console.log(f)
+
+var c = 20;
+
+if(true){
+    let y = 20;
+    let x = 30;
+
+    console.log(y, x, c);
+}
+console.log(c);
