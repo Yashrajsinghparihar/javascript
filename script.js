e84408ed7869bdf3b951f67eac642be6126df4f7
Yashraj Singh Parihar <yashrajsingh9340@gmail.com>
2026-09-22 10:59:16 +0530

    js operator day2

--- script.js
+++ script.js
@@ -9,12 +9,12 @@
 
 // var 
 
-var a = 10;
-console.log(a);
-var a = 20;
+// var a = 10;
+// console.log(a);
+// var a = 20;
 // console.log(a)
-a = 21;
-console.log(a)
+// a = 21;
+// console.log(a)
 
 // let b = 30;
 // b = 40;
@@ -26,37 +26,101 @@ console.log(a)
 
 // console.log(f)
 
-var c = 20;
+// var c = 20;
 
-if(true){
-    let y = 20;
-    let x = 30;
+// if(true){
+//     let y = 20;
+//     let x = 30;
 
-    console.log(y, x, c);
-}
-console.log(c);
+//     console.log(y, x, c);
+// }
+// console.log(c);
 
 // data two types
 // primitive:
 
-let h = "hello"
-console.log(typeof(h))
-let v = 45;
-console.log(typeof(v))
+// let h = "hello"
+// console.log(typeof(h))
+// let v = 45;
+// console.log(typeof(v))
 
 // Non- Primitve:
 
 // Array
-let fruits = ["Apple", "Mango", "orange"]
-console.log(fruits)
+// let fruits = ["Apple", "Mango", "orange"]
+// console.log(fruits)
 
 // object:
 
-let student = {
-    name: "yashraj",
-    age: 30
+// let student = {
+//     name: "yashraj",
+//     age: 30
+// }
+// console.log(student)
+
+
+// operator
+
+// Arithmetic operator:
+
+let a = 10;
+let b = 20;
+
+console.log(a+b)  //addition
+console.log(a-b) //substraction
+console.log(a*b) // multiplication
+console.log(a/b) // divide
+console.log(a%b) // reminder
+console.log(a**b) //power
+
+
+// comparision operator
+
+console.log(10 > 4); //true
+console.log(10 < 4); // flase
+console.log(10 >= 4); // true
+console.log(10 <= 4); //false
+
+// equality
+console.log(10 == "10") //true
+// enquality
+console.log(10 === "10") // false
+
+// Assignment operator
+
+let score = 10;
+score += 5;
+score -= 5;
+score *= 5;
+score /= 5;
+
+console.log(score)
+
+// grouping
+
+console.log(3 + 4 * 4);
+
+// logical operator;
+
+let marks = 50;
+let attendence = 60;
+
+if(marks >= 40 && attendence >= 75){
+    console.log("you can give the exam")
+}else{
+    console.log("you are not allowed")
 }
-console.log(student)
+
+// string
+
+let message = " hello javascript "
+
+console.log(message.length);
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+console.log(message.slice());
+console.log(message.trim());
+
 
 
 
